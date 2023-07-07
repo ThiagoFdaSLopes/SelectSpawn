9bd0481abc86a98c6c192e5e54198deef825bf14
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-07 15:19:33 -0300

    feat: List Imgs

---
+++ src/Mocks/listImgs.ts
@@ -0,0 +1,11 @@
+import femaleBG from '../Images/female1.png';
+import femaleBG2 from '../Images/female2.png';
+import female3 from '../Images/female3.png';
+
+import masculine1 from '../Images/masculine1.png';
+import masculine2 from '../Images/masculine2.png';
+import masculine3 from '../Images/masculine3.png';
+
+export const listPlayers = [
+    [femaleBG, femaleBG2, female3],[masculine1, masculine2, masculine3]
+]
