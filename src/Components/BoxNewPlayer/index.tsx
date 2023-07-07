0e0604fcbe6d0b282c10cf0278754172c0e7b868
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-07 16:16:13 -0300

    feat: Criar personagem New

--- src/Components/BoxNewPlayer/index.tsx
+++ src/Components/BoxNewPlayer/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { ButtonPlay, MainBox, MainCardName } from './style';
+import { ButtonPlay, MainBox, MainCardInfoNewPlayer, SpaceFree, SpaceFreeContent } from './style';
 
 const BoxNewPlayer: React.FC = () => {
   return (
     <MainBox>
-        <MainCardName>
-          <p>Create New Player</p>
-        </MainCardName>
+      <MainCardInfoNewPlayer>
+        <SpaceFree><p>Espaço Livre</p></SpaceFree>
+        <SpaceFreeContent><p>Você pode criar personagem gratuitamente. Após a ativação, você será redirecionado para o editor de personagens, após o qual começará a vida do zero.</p></SpaceFreeContent>
+      </MainCardInfoNewPlayer>
         <ButtonPlay>
-          Create
+          CRIAR PERSONAGEM
         </ButtonPlay>
     </MainBox>
   )
