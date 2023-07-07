36235c633ecfe169a1a58fa58274ba3e8fb31448
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-07 12:14:50 -0300

    feat: MainBG

---
+++ src/Components/style.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MainBG = styled.main`
+    width: 120rem;
+    height: 67.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: url('../src/Images/fireBG.png');
+    background-repeat: no-repeat;
+    overflow: hidden;
+`;
