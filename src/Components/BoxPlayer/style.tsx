a95b016cdf6e6c7e5de38e0650442b0d3584e911
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-07 15:19:27 -0300

    fix: Style

--- src/Components/BoxPlayer/style.tsx
+++ src/Components/BoxPlayer/style.tsx
@@ -1,17 +1,23 @@
 import styled from "styled-components";
 
-export const MainBox = styled.div`
-    display: flex;
+type PropsImgs = {
+    img: string,
+}
+
+export const MainBox = styled.div<PropsImgs>`
     width: 40rem;
     height: 67.5rem;
-
-    background: rgba(28, 28, 28, 0.84);
+    background: rgba(41, 39, 39, 0.84);
+    background-image: url(${({ img }) => img});
+    background-repeat: no-repeat;
+    background-position: right 14rem;
     overflow: hidden;
+    display: flex;
 `;
 
 export const MainCardName = styled.section`
-    width: 6.6875rem;
-    height: 44.3125rem;
+    width: 6.5rem;
+    height: 100vh;
     background: linear-gradient(180deg, rgba(226, 226, 226, 0.06) 0%, rgba(0, 0, 0, 0.06) 100%);
     display: flex;
     align-items: center;
@@ -36,13 +42,22 @@ export const ButtonPlay = styled.button`
     border-radius: 0.25rem;
     border: 1px solid #FDD330;
     background: linear-gradient(1deg, rgba(253, 211, 48, 0.44) 0%, rgba(253, 211, 48, 0.88) 100%);
-
+    
     position: relative;
-    left: 2rem;
+    left: 1rem;
     top: 55rem;
+    z-index: 3;
     color: #FFF;
     font-size: 1.5rem;
     font-style: normal;
     font-weight: 400;
     line-height: normal;
 `;
+
+// export const MainImage = styled.img<PropsImgs>`
+//     width: 30rem;
+//     height: 32rem;
+//     border: none;
+//     background-image: url(${({ img }) => img});
+//     background-repeat: no-repeat;
+// `;
