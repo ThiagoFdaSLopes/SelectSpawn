140c13c2d241bbdc12699d9f35c8bd75b6baa191
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-07 16:16:39 -0300

    feat: newStyled

--- src/Components/BoxNewPlayer/style.tsx
+++ src/Components/BoxNewPlayer/style.tsx
@@ -4,30 +4,57 @@ export const MainBox = styled.div`
     display: flex;
     width: 40rem;
     height: 67.5rem;
-
     background: rgba(28, 28, 28, 0.84);
     overflow: hidden;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 `;
 
-export const MainCardName = styled.section`
-    width: 6.5rem;
-    height: 100vh;
-    background: linear-gradient(180deg, rgba(226, 226, 226, 0.06) 0%, rgba(0, 0, 0, 0.06) 100%);
+export const SpaceFree = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 22.5625rem;
+  height: 3.375rem;
+
+  p {
+    font-family: "Gilroy-Medium";
+    font-size: 2.5rem;
+    color: white;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
+
+export const SpaceFreeContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 22.5625rem;
+  height: 3.375rem;
+  margin-top: 1rem;
+
+  p {
+    font-family: "Gilroy-Medium";
+    font-size: 1rem;
+    color: white;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    text-align: center;
+  }
+`;
+
+export const MainCardInfoNewPlayer = styled.section`
+    width: 30rem;
+    height: 20rem;
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
     overflow: hidden;
-
-    p {
-        width: 25rem;
-        transform: rotate(-90deg);
-        font-size: 2.5rem;
-        font-style: normal;
-        font-weight: 700;
-        line-height: normal;
-        color: #FFF;
-    }
 `;
 
 export const ButtonPlay = styled.button`
@@ -37,10 +64,11 @@ export const ButtonPlay = styled.button`
     border: 1px solid #93fd30;
     background: linear-gradient(1deg, rgba(164, 253, 48, 0.44) 0%, rgba(123, 253, 48, 0.88) 100%);
 
-    position: relative;
-    left: 2rem;
-    top: 55rem;
     color: #FFF;
+    position: absolute;
+    bottom: 8.5rem;
+    font-family: "Gilroy-Bold";
+    text-transform: uppercase;
     font-size: 1.5rem;
     font-style: normal;
     font-weight: 400;
