ec3d00dd222599c1dd212e9b278e95906ffd9fc6
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-07 11:14:17 -0300

    feat: Removed ResetCss

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { VisibilityProvider } from './providers/VisibilityProvider';
-import App from './components/App';
+import App from './Components/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <VisibilityProvider>
-      <ResetCss />
         <App />
     </VisibilityProvider>
   </React.StrictMode>,
