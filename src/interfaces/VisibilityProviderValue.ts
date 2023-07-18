7cc4c3aa9d6b418c7ad3c58a03aacd1930fd3e86
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-18 18:01:18 -0300

    feat: Add Props Interface

--- src/interfaces/VisibilityProviderValue.ts
+++ src/interfaces/VisibilityProviderValue.ts
@@ -2,6 +2,8 @@ import Player from "./Player"
 
 export default interface VisibilityProviderValue {
     setVisible: (visible: boolean) => void
+    setVisibleSpawnSelect: (visibleSpawnSelect: boolean) => void
     visible: boolean
+    visibleSpawnSelect: boolean
     playersInfo: Player[]
 }
