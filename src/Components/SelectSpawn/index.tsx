af08bb8b7d7f9e6ce9d20f3145b426c036f217b8
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-19 13:15:47 -0300

    feat: SelectSpawn

--- src/Components/SelectSpawn/index.tsx
+++ src/Components/SelectSpawn/index.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { MainSelectSpawn, MainBox, Section, H1Title, ButtonLastLocation, ButtonSpawnar } from './style';
+import playerOptionsSpawn from '../../Mocks/playerOptinsSpawn.json';
+import { fetchNui } from '../../utils/fetchNui';
 
 const SelectSpawn: React.FC = () => {
+  const [selectSpawn, setselectSpawn] = useState(false)
+  const [selectPosition, setselectPosition] = useState("")
+
+  const handleSelectSpawn = (position: any) => {
+    setselectSpawn(!selectSpawn)
+    setselectPosition(position)
+  }
+
+  const handleSpawnar = () => {
+    // fetchNui()
+  }
   return (
-    <div>SelectSpawn</div>
+    <MainSelectSpawn>
+      <H1Title>SELECT YOUR SPAWN</H1Title>
+        <Section>
+          {
+            playerOptionsSpawn.map((option, index) => 
+            <MainBox key={index}>
+              <h1>{option.title}</h1>
+              <div className='image'>
+                <img src={option.image} alt="location spawn" />
+              </div>
+              <div className='text'>{option.text}</div>
+              <button className='button' onClick={() => handleSelectSpawn(option.position)}>Select Spawn</button>
+            </MainBox>
+            )
+          }
+        </Section>
+      {
+        selectSpawn ? (
+          <ButtonSpawnar onClick={handleSpawnar}>Spawnar</ButtonSpawnar>
+        ) :
+        (
+          <ButtonLastLocation>Last Location</ButtonLastLocation>
+        )
+      }
+    </MainSelectSpawn>
   )
 }
 
