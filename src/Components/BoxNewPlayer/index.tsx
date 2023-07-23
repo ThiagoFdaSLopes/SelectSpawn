cf43568226cdbc203d7546e2fa37b86f2a4abcc5
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-23 17:59:53 -0300

    feat: Criador

--- src/Components/BoxNewPlayer/index.tsx
+++ src/Components/BoxNewPlayer/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { ButtonPlay, MainBox, MainCardInfoNewPlayer, SpaceFree, SpaceFreeContent } from './style';
+import { useVisibility } from '../../providers/VisibilityProvider';
+import { fetchNui } from '../../utils/fetchNui';
 
 const BoxNewPlayer: React.FC = () => {
+
+  const handleBoxCreator = () => {
+    fetchNui("OpenCreator")
+  }
+
   return (
     <MainBox>
       <MainCardInfoNewPlayer>
         <SpaceFree><p>Espaço Livre</p></SpaceFree>
         <SpaceFreeContent><p>Você pode criar personagem gratuitamente. Após a ativação, você será redirecionado para o editor de personagens, após o qual começará a vida do zero.</p></SpaceFreeContent>
       </MainCardInfoNewPlayer>
-        <ButtonPlay>
+        <ButtonPlay onClick={handleBoxCreator}>
           CRIAR PERSONAGEM
         </ButtonPlay>
     </MainBox>
