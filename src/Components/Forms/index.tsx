af1f250cb8b7ab29ba0725d0e033a1163b77baef
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-24 12:11:05 -0300

    feat: Component Forms

---
+++ src/Components/Forms/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { InputMain, MaiNForms } from './style'
+
+const Forms = () => {
+  return (
+    <MaiNForms>
+      <InputMain placeholder='First Name'/>
+      <InputMain placeholder='Last Name'/>
+    </MaiNForms>
+  )
+}
+
+export default Forms
