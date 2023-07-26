14f5c7562c473c88b9ac92bdbf493e3ca3304fcf
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-26 14:19:46 -0300

    feat: Component Forms Edit

--- src/Components/Forms/index.tsx
+++ src/Components/Forms/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputMain, MaiNForms } from './style'
+import { useVisibility } from '../../providers/VisibilityProvider';
 
-const Forms = () => {
+const Forms: React.FC = () => {
+
+  const { setfirst, setlast, first, last } = useVisibility();
+
+  interface InputEvent {
+    target: {
+      name: string;
+      value: string;
+    };
+  }
+
+  const handleInput = (event: InputEvent ) => {
+    event.target.name === "first" ? setfirst(event.target.value) : setlast(event.target.value)
+  }
   return (
     <MaiNForms>
-      <InputMain placeholder='First Name'/>
-      <InputMain placeholder='Last Name'/>
+      <InputMain placeholder='First Name' name="first" onChange={handleInput} value={first}/>
+      <InputMain placeholder='Last Name' name="last" onChange={handleInput} value={last}/>
     </MaiNForms>
   )
 }
