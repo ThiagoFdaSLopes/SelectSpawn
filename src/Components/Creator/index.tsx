266da903d7ca5a7c1a4caf1d6cca89d07cd98419
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-26 14:19:24 -0300

    feat: Creator Basic

--- src/Components/Creator/index.tsx
+++ src/Components/Creator/index.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import { MainCreator, MenuLeft, MenuRigth } from './style'
+import React, { useState } from 'react'
+import { ButtonCreatePerson, ButtonGenderOption, ButtonReturn, GenderSelect, MainCreator, MenuLeft, MenuRigth } from './style'
 import Forms from '../Forms'
+import masculine from '../../Images/silhuetamasculine.png';
+import feminine from '../../Images/silhuetafeminine.png';
+import { useVisibility } from '../../providers/VisibilityProvider';
+import { fetchNui } from '../../utils/fetchNui';
 
 const Creator: React.FC = () => {
+  const [gender, setGender] = useState("")
+  const [activeButton, setActiveButton] = useState('buttonf');
+  const { first, last } = useVisibility()
+
+  const handleGender = async (option: string, buttonName: string) => {
+    setActiveButton(buttonName);
+    setGender(option)
+    await fetchNui("CustomizationChar", { Skin: gender, Tattos: [], Barber: [], Clothes: []})
+  }
+
+  const handleReturn = () => {
+    fetchNui("ReturnPlayerList")
+  }
+
+  const handleCreatePerson = () => {
+    fetchNui("NewCharacter", { name: first, lastname: last, sex: gender})
+  }
   return (
     <MainCreator>
       <MenuRigth>
         <h1>CHARACTER CREATION</h1>
         <Forms />
+        <GenderSelect>
+          <ButtonGenderOption color='249, 52, 121, 0.80' active={activeButton === 'buttonf'} onClick={() => handleGender("mp_f_freemode_01", "buttonf")} >
+            <img src={feminine} alt="silhueta feminine" />
+          </ButtonGenderOption>
+          <ButtonGenderOption color='78, 52, 249, 0.80' active={activeButton === 'buttonm'} onClick={() => handleGender("mp_m_freemode_01", "buttonm")}>
+            <img src={masculine} alt="silhueta masculine" />
+          </ButtonGenderOption>
+        </GenderSelect>
+        <ButtonCreatePerson onClick={handleCreatePerson}>
+          CREATE
+        </ButtonCreatePerson>
+        <ButtonReturn onClick={handleReturn}>
+          RETURN
+        </ButtonReturn>
       </MenuRigth>
       <MenuLeft />
     </MainCreator>
