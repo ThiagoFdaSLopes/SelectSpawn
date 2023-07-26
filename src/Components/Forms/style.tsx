ce43422224d554c06a2bbe303f47a68673b42294
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-26 14:19:51 -0300

    feat: Component Forms Edit

--- src/Components/Forms/style.tsx
+++ src/Components/Forms/style.tsx
@@ -11,12 +11,20 @@ export const MaiNForms = styled.form`
 `;
 
 export const InputMain = styled.input`
-    width: 20rem;
-    border: solid 1.5px #9e9e9e;
+    width: 23rem;
+    border: 2px solid #9e9e9e;
     background: none;
     padding: 1rem;
     font-size: 1rem;    
     color: #f5f5f5;
     transition: border 150ms cubic-bezier(0.4,0,0.2,1);
     margin-bottom: 10px;
+
+    color: #FFF;
+    font-family: "Gilroy-Bold";
+    text-transform: uppercase;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
 `;
