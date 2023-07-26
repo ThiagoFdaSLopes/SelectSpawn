34c5acbdc1aa051ba8ac403c53f74923044e4e92
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-26 20:53:54 -0300

    feat: Spawn Option

--- src/Components/SelectSpawn/index.tsx
+++ src/Components/SelectSpawn/index.tsx
@@ -6,10 +6,12 @@ import { useVisibility } from '../../providers/VisibilityProvider';
 
 const SelectSpawn: React.FC = () => {
   const { playerChose } = useVisibility()
+  const [activeButton, setActiveButton] = useState(-10)
   const [selectSpawn, setselectSpawn] = useState(false)
   const [selectPosition, setselectPosition] = useState("")
 
   const handleSelectSpawn = (position: any, index: number) => {
+    setActiveButton(index - 1)
     setselectSpawn(true)
     setselectPosition(position)
     const data = { index: index, selectPosition, playerChose }
@@ -26,13 +28,13 @@ const SelectSpawn: React.FC = () => {
         <Section>
           {
             playerOptionsSpawn.map((option, index) => 
-            <MainBox key={index}>
+            <MainBox key={index} active={activeButton === index}>
               <h1>{option.title}</h1>
               <div className='image'>
                 <img src={option.image} alt="location spawn" />
               </div>
               <div className='text'>{option.text}</div>
-              <button className='button' onClick={() => handleSelectSpawn(option.position, index + 1)}>Select Spawn</button>
+              <button className='button' onClick={() => handleSelectSpawn(option.position, index + 1, )}>Select Spawn</button>
             </MainBox>
             )
           }
